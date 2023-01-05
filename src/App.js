8ee385e8d5c1dcd42038ab7337247856efb517bf
CKURZ <ckurz098@gmail.com>
2023-01-05 11:50:28 -0800

    Added save functionality that generates file compatible with Freds software

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { scadGen } from './resources/SCAD-GEN';
-import { musicFormatExample } from './resources/noteKey';
+import { scadGen } from './util/SCAD-GEN';
+import { musicFormatExample } from './util/noteKey';
 import { Staff } from './components/Staff/Staff.component';
 import { Parameters } from './components/Parameters/Parameters';
 
@@ -10,28 +10,27 @@ import { PlaySongButton } from './components/PlaySongButton/PlaySongButton.compo
 import { RecordPreview } from './components/RecordPreview/RecordPreview.component';
 import { SongTitle } from './components/SongTitle/SongTitle.component';
 
+import { downloadSave, downloadScad } from './util/saving';
+
 
 function App() {
 
   const [notes, setNotes] = useState(musicFormatExample);
   const [maxBeats, setMaxBeats] = useState(80);
   const [song, setSong] = useState(0);
-  const [mousePos, setMousePos] = useState([0,0])
+  const [mousePos, setMousePos] = useState([0, 0])
 
   useEffect(() => {
     console.log('ReRender matrix: ', maxBeats)
-
     setNotes(old => {
       const title = old[song].pop();
       const length = old[song][0].length;
-
       if (maxBeats > length) {
         const numToAdd = maxBeats - length;
         for (let i = 0; i < 16; i++) {
           const addition = Array(numToAdd).fill(0);
           old[song][i] = old[song][i].concat(addition);
         }
-
 
       } else if (maxBeats < length) {
         const numToSub = length - maxBeats;
@@ -52,34 +51,35 @@ function App() {
     setMaxBeats(notes[song][0].length)
   }, [song])
 
+  //load notes from local storage if they are there
+  useEffect(() => {
+    if (localStorage.getItem('savedNotes')) {
+      const savedNotes = JSON.parse(localStorage.getItem('savedNotes'))
+      setNotes(savedNotes);
+    } else {
+      localStorage.setItem('savedNotes', JSON.stringify(notes));
+    };
 
-  function download(textData) {
-    const generatedFile = textData;
-    let blob = new Blob([generatedFile.text], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
+  }, [])
+  //save notes to local storage on change
+  useEffect(() => {
+   localStorage.setItem('savedNotes',JSON.stringify(notes));
+  }, [notes])
 
 
-    var element = document.createElement('a');
-    element.setAttribute('href', url);
-    element.setAttribute('download', `${generatedFile.title}.scad`);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-  };
 
   return (
     <div className="App">
       <h1>Fisher Price Record Maker</h1>
-      <SongTitle notes= {notes} setNotes = {setNotes} song = {song} setSong = {setSong} />
+      <SongTitle notes={notes} setNotes={setNotes} song={song} setSong={setSong} />
       <Parameters setMaxBeats={setMaxBeats} maxBeats={maxBeats} />
-      <Staff notes={notes} song={song} setNotes={setNotes} mousePos={mousePos} setMousePos = {setMousePos} />
-      <PlaySongButton notes = {notes} song={song} maxBeats={maxBeats} mousePos={mousePos} setMousePos = {setMousePos}/>
-      <RecordPreview notes = {notes} maxBeats={maxBeats} song={song} mousePos={mousePos}/>
-      <button onMouseDown={() => download(scadGen(notes))} >Download SCAD File</button>
+      <Staff notes={notes} song={song} setNotes={setNotes} mousePos={mousePos} setMousePos={setMousePos} />
+      <PlaySongButton notes={notes} song={song} maxBeats={maxBeats} mousePos={mousePos} setMousePos={setMousePos} />
+      <RecordPreview notes={notes} maxBeats={maxBeats} song={song} mousePos={mousePos} />
+      <button onMouseDown={() => downloadScad(scadGen(notes))} >Download SCAD File</button>
+      <button onMouseDown={() => downloadSave(notes[song])} >Save This Song</button>
+  
     </div>
   );
 }
---
+++ src/util/saving.js
@@ -0,0 +1,56 @@
+export const downloadSave = (notes) => {
+    
+    let generatedFile = '';
+    for(let i = 15 ; i >=0 ; i--){
+        if (i !== 15) generatedFile += '\n';
+        // eslint-disable-next-line no-loop-func
+        notes[i].forEach((beat) => {
+          
+            if (beat) {
+                generatedFile += '+';
+            } else{
+                generatedFile += '-';
+            }
+        });
+    };
+
+    generatedFile += `\n${notes[16]}`
+
+    console.log(generatedFile);
+
+
+    let blob = new Blob([generatedFile], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+
+    var element = document.createElement('a');
+    element.setAttribute('href', url);
+    element.setAttribute('download', `${notes[16]}.fpr`);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+
+
+};
+
+export function downloadScad(textData) {
+    const generatedFile = textData;
+    let blob = new Blob([generatedFile.text], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+
+    var element = document.createElement('a');
+    element.setAttribute('href', url);
+    element.setAttribute('download', `${generatedFile.title}.scad`);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  };
