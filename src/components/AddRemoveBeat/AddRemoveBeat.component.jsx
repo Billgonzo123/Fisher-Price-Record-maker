14992f3598ad227552a76cadb2ddc9dea2311987
CKURZ <ckurz098@gmail.com>
2023-01-10 20:30:16 -0800

    Adjusted button position and placed in div container

--- src/components/AddRemoveBeat/AddRemoveBeat.component.jsx
+++ src/components/AddRemoveBeat/AddRemoveBeat.component.jsx
@@ -9,9 +9,9 @@ const removeBeat = () => {
     setMaxBeats(old => (old-1))
 }
     return (
-        <>
+        <div className='add-remove-container'>
         <button className="add-remove-button" type="button" onClick={removeBeat}>- </button>
         <button className="add-remove-button" type="button" onClick={addBeat}>+ </button>
-        </>
+        </div>
     )
 }
