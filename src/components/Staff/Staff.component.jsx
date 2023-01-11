4eddbb72d49285b3bb94c66a9b2fb9e9d1ce15fa
CKURZ <ckurz098@gmail.com>
2023-01-10 22:14:48 -0800

    Fixed bug when shifting that tried to add values to the title data

--- src/components/Staff/Staff.component.jsx
+++ src/components/Staff/Staff.component.jsx
@@ -1,6 +1,7 @@
 import { AddRemoveBeat } from "../AddRemoveBeat/AddRemoveBeat.component";
 import { BeatLine } from "../BeatLine/BeatLine.component";
 import { NotesDisplay } from "../NotesDisplay/NotesDisplay.component";
+import { ShiftCol } from "../ShiftCol/ShiftCol.component";
 import "./staff.css";
 
 export const Staff = ({ notes, song, setNotes, mousePos, setMousePos, setMaxBeats }) => {
@@ -8,13 +9,16 @@ export const Staff = ({ notes, song, setNotes, mousePos, setMousePos, setMaxBeat
         <div className='staff-container'>
             <div className={"staff-component"}>
                 <NotesDisplay />
+
                 {notes[song].map((note, i) => {
                     //we dont want to render a staff line for the last value in the note array as it is the songs title, not a note
                     if (i === 16) return "";
-
+                    
                     //render each beat
                     return note.map((beat, j) => {
                         return (
+                            <>
+                           {(i === 0) ? <button type='button' style={{gridColum: `${j}`, gridRow: '-2'}}></button>: ''}
                             <BeatLine
                                 key={`beat-${i}-${j}-${beat}`}
                                 beat={beat}
@@ -24,9 +28,12 @@ export const Staff = ({ notes, song, setNotes, mousePos, setMousePos, setMaxBeat
                                 mousePos={mousePos}
                                 setMousePos={setMousePos}
                             />
+                            {  (mousePos[1] === j) ? <ShiftCol  setMousePos = {setMousePos} setNotes={setNotes} song = {song} mousePos={mousePos}/> : '' }
+                            </>
                             
                         );
                     });
+                  
                 })}
             </div>
             <AddRemoveBeat setMaxBeats={setMaxBeats}/>
